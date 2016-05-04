e3464d3674cbd53b1c768f831e4f357df686a414
JorgeIgnacio <JorgeIgnacio@Jupiter>
2016-05-04 08:03:39 -0600

    FRM_MenuPrincipal

---
+++ tarea02/src/controlador/Controlador_FRM_MenuPrincipal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author JorgeIgnacio
+ */
+public class Controlador_FRM_MenuPrincipal implements ActionListener {
+    
+    //Referencias
+    //Constructor
+    public Controlador_FRM_MenuPrincipal() {
+        //Instancias
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+}//Fin Controlador_FRM_MenuPrincipal
